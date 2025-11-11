1a70d801950c8a3afa1377c6f859d41a46c18234
steven42069 <stevendiaz152@gmail.com>
2025-11-10 23:21:36 -0500

    one more

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def copy_file(command: str) -> None:
     if source_file == destination_file:
         return
     try:
-        with open(source_file, "r") as f_in, open(destination_file, "w") as f_out:
+        with open(source_file, "r") as f_in, \
+             open(destination_file, "w") as f_out:
             f_out.write(f_in.read())
     except FileNotFoundError:
         return
