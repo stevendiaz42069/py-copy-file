419de9a5fb71e0cfa61ebaaa613820fae1e7ed38
steven42069 <stevendiaz152@gmail.com>
2025-11-10 23:10:10 -0500

    done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+    if parts[1] == parts[2]:
+        return
+    try:
+        with open(parts[1], "r") as f_in, open(parts[2], "w") as f_out:
+            f_out.write(f_in.read())
+    except FileNotFoundError:
+        return
